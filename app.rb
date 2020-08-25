de4c8a04f6676024a2c1ccf8e943ed931aab0e6c
chic-page-6502 <zacharytq@gmail.com>
2020-08-25 12:52:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @result = :num1.to_i * :num2.to_i
+    @result = params[:num1].to_i * params[:num2].to_i
     "#{@result.to_s}"
   end
 
